d8a3c1222f0adf5d403ae2cf173844e8f23c234d
Moshe Kamensky <moshe.kamensky@gmail.com>
2023-07-15 14:59:01 +0300

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,37 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+    es2022: true,
+    amd: true,
+  },
+  plugins: [
+    'vue',
+    'vue-i18n',
+  ],
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/base',
+    'plugin:vue/vue3-essential',
+    'plugin:vue/vue3-recommended',
+    'plugin:vue/vue3-strongly-recommended',
+    'prettier',
+    'plugin:prettier/recommended',
+  ],
+  globals: {},
+  rules: {
+    'vue/no-unused-vars': 'error',
+    'vue/script-setup-uses-vars': 'error',
+    'vue/multi-word-component-names': 'off',
+    'vue/require-default-prop': 'off',
+    'vue/no-reserved-component-names': 'off',
+    'vue/no-v-html': 'off',
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+  },
+  parserOptions: {
+    sourceType: 'module', // allow the use of imports statements
+    ecmaVersion: 2020, // allow the parsing of modern ecmascript
+  },
+};
