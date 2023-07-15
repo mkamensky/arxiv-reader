863c9b6589d1461b228731055ad145783de88e64
Moshe Kamensky <moshe.kamensky@gmail.com>
2023-07-15 16:04:59 +0300

    quasar

--- app/frontend/entrypoints/application.js
+++ app/frontend/entrypoints/application.js
@@ -18,11 +18,27 @@ console.log('Visit the guide for more information: ', 'https://vite-ruby.netlify
 // import * as Turbo from '@hotwired/turbo'
 // Turbo.start()
 //
-// import ActiveStorage from '@rails/activestorage'
-// ActiveStorage.start()
+import ActiveStorage from '@rails/activestorage'
+ActiveStorage.start()
 //
 // // Import all channels.
 // const channels = import.meta.globEager('./**/*_channel.js')
 
 // Example: Import a stylesheet in app/frontend/index.css
 // import '~/index.css'
+
+import { createApp, h } from 'vue'
+import { createInertiaApp } from '@inertiajs/vue3'
+
+createInertiaApp({
+  resolve: name => {
+    const pages = import.meta.glob('./Pages/**/*.vue', { eager: true })
+    return pages[`./Pages/${name}.vue`]
+  },
+  setup({ el, App, props, plugin }) {
+    createApp({ render: () => h(App, props) })
+      .use(plugin)
+      .mount(el)
+  },
+})
+
