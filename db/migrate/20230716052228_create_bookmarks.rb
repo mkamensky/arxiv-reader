7c40edc72db09e28590856e78b280088b5b5ee49
Moshe Kamensky <moshe.kamensky@gmail.com>
2023-07-16 08:38:26 +0300

    recommendations

--- db/migrate/20230716052228_create_bookmarks.rb
+++ db/migrate/20230716052228_create_bookmarks.rb
@@ -1,11 +1,11 @@
 class CreateBookmarks < ActiveRecord::Migration[7.0]
   def change
     create_table :bookmarks do |t|
-      t.references :paper, null: false, foreign_key: true
-      t.references :user, null: false, foreign_key: true
+      t.references :paper, null: false, foreign_key: true, index: true
+      t.references :user, null: false, foreign_key: true, index: true
 
       t.timestamps
     end
+    add_index :bookmarks, %i[paper_id user_id], unique: true
   end
-  add_index :bookmarks, %i[paper_id user_id], unique: true
 end
