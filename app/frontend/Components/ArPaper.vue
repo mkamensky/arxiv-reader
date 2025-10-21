247eefeaad75374092de802e9a061680079dfe87
Moshe Kamensky <moshe.kamensky@gmail.com>
2025-10-21 10:41:29 +0300

    render latex in msc tooltips

--- app/frontend/Components/ArPaper.vue
+++ app/frontend/Components/ArPaper.vue
@@ -142,7 +142,7 @@
                     dense
                     size="sm"
                   >
-                    {{ tag }}<q-tooltip>{{ $mdi($options.msc[tag]) }}</q-tooltip>
+                    {{ tag }}<q-tooltip v-html="$mdi($options.msc[tag])" />
                   </q-chip>
                 </q-item-label>
               </q-item-section>
@@ -159,7 +159,7 @@
                     dense
                     size="sm"
                   >
-                    {{ tag }}<q-tooltip>{{ $mdi($options.msc[tag]) }}</q-tooltip>
+                    {{ tag }}<q-tooltip v-html="$mdi($options.msc[tag])" />
                   </q-chip>
                 </q-item-label>
               </q-item-section>
