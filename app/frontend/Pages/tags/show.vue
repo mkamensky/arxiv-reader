84ffa2206c17f4c5f36e6cc73326356093efad98
Moshe Kamensky <moshe.kamensky@gmail.com>
2026-01-02 13:15:21 +0200

    add the tag show page

---
+++ app/frontend/Pages/tags/show.vue
@@ -0,0 +1,38 @@
+<template>
+  <q-page padding class="q-pt-xl">
+    <div v-if="papers?.length" class="row">
+      <ar-paper
+        v-for="paper in papers"
+        :key="paper.id"
+        :object="paper"
+        class="q-pa-md col-12 col-lg-6"
+      />
+    </div>
+    <div v-else>
+      Nothing yet!
+    </div>
+  </q-page>
+</template>
+
+<script>
+import ArPaper from '@/Components/ArPaper.vue'
+
+export default {
+  components: {
+    ArPaper,
+  },
+
+  props: {
+    tag: Object,
+  },
+  data() {
+    return {
+    }
+  },
+  computed: {
+    papers() {
+      return this.tag.papers
+    },
+  },
+}
+</script>
